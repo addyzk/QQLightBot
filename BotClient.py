6a2381b8f3a2f91aa0972dc2eb72d9396221c74a
Irony <892768447@qq.com>
2019-02-21 22:48:34 +0800

    first commit

---
+++ BotClient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2019年2月21日
+@author: Irony
+@site: https://pyqt5.com https://github.com/892768447
+@email: 892768447@qq.com
+@file: BotClient
+@description: 机器人WS客户端
+"""
+
+__Author__ = 'Irony'
+__Copyright__ = 'Copyright (c) 2019'
